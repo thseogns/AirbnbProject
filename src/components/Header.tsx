166cf7414ae458f5538ae44e964ffba392132198
thseogns <thseo1758@naver.com>
2024-02-13 23:53:30 +0900

    checkin and navbar styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,43 @@
 import React from "react";
 import styles from "./Header.module.css";
-import Navigation from "./header/Navigation";
+import Navigation from "./headerNav/Navigation";
 import Searchbar from "./searchbar/Searchbar";
+import ButtonComponent from "./ButtonComponent";
 
 const Header = () => {
   return (
     <div className={styles.headerMargin}>
       <div className={styles.header}>
-        <div className={styles.logoImage}>로고이미지</div>
-        <div className={styles.checkIn}>
-          <div>
-            <button>
-              <span>숙소</span>
-            </button>
-          </div>
-          <div>
-            <button>
-              <span>체험</span>
-            </button>
-          </div>
-          <div>
-            <a href="#">온라인 체험</a>
+        <div className={styles.logoImage}>
+          <a>
+            <img src={process.env.PUBLIC_URL + "/logo.png"} alt="로고" />
+          </a>
+        </div>
+        <div className={styles.position}>
+          <div className={styles.checkInCover}>
+            <div className={styles.checkIn}>
+              <div className={styles.lodging}>
+                <ButtonComponent>
+                  <span>숙소</span>
+                </ButtonComponent>
+              </div>
+              <div className={styles.focus}>
+                <ButtonComponent>
+                  <span>체험</span>
+                </ButtonComponent>
+              </div>
+              <div className={styles.focus}>
+                <a href="#">
+                  <span>온라인 체험</span>
+                </a>
+              </div>
+            </div>
+            <Searchbar />
           </div>
         </div>
 
         <Navigation />
       </div>
-      <Searchbar />
     </div>
   );
 };
--- src/components/searchbar/Searchbar.tsx
+++ src/components/searchbar/Searchbar.tsx
@@ -3,12 +3,15 @@ import styles from "./Searchbar.module.css";
 const Searchbar = () => {
   return (
     <div className={styles.searchbarCover}>
-      <div>여행지</div>
+      <div className={styles.travelDestination}>여행지</div>
       <div className={styles.checkInAndOut}>
         <div>체크인</div>
         <div>체크아웃</div>
       </div>
-      <div>여행자</div>
+      <div className={styles.traveler}>
+        <div className={styles.guest}>여행자</div>
+        <div>이미지</div>
+      </div>
     </div>
   );
 };
