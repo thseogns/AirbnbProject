8c1bfa14c37892ff5499775e114d0e2a4d3311b9
thseogns <thseo1758@naver.com>
2024-02-18 06:07:57 +0900

    create prev and next arrow

--- src/components/menubar/Menubar.tsx
+++ src/components/menubar/Menubar.tsx
@@ -2,11 +2,29 @@ import React from "react";
 import MenuItems from "./MenuItems";
 import styles from "./Menubar.module.css";
 const Menubar = () => {
+  const slideWidth = React.useRef<HTMLDivElement>(null);
+  const slideHandler = () => {
+    console.log(slideWidth.current?.clientWidth);
+  };
   return (
     <div className={styles.menubarCover}>
-      <div className={styles.scroll}>
+      <div className={styles.scroll} ref={slideWidth}>
+        <div className={styles.arrowButton} onClick={slideHandler}>
+          <div>
+            <div>
+              <img src={process.env.PUBLIC_URL + "/arrow_button.png"} />
+            </div>
+          </div>
+        </div>
         <div>
           <MenuItems />
+        </div>
+        <div className={styles.arrowButton}>
+          <div>
+            <div>
+              <img src={process.env.PUBLIC_URL + "/arrow_button.png"} />
+            </div>
+          </div>
         </div>
       </div>
       <div className={styles.filterButton}>
