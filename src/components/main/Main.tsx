0ca297200a9d9b094e4715455c743e8d6e56629b
thseogns <thseo1758@naver.com>
2024-02-20 19:32:26 +0900

    make frame of the mainComponent

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,7 +1,18 @@
+/** @jsxImportSource @emotion/react */
 import React from "react";
-
+import { css } from "@emotion/react";
+import { grid, gridColonm5 } from "../../emotion/grid";
 const Main = () => {
-  return <div>Main</div>;
+  const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  return (
+    <ul css={[grid, gridColonm5, { width: "90%", margin: "auto" }]}>
+      {number.map((index) => (
+        <li key={index} css={{ listStyle: "none" }}>
+          aa
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Main;
--- src/emotion/grid.ts
+++ src/emotion/grid.ts
@@ -2,3 +2,11 @@ import { css } from "@emotion/react";
 export const grid = css`
   display: grid;
 `;
+export const gridColonm5 = css`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+`;
+export const gridRow3 = css`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+`;
